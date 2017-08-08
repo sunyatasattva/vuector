64c6f30a21e6836a3746d6b9e8ab61f7c4fefac5
Veren <sunyatasattva@gmail.com>
2017-08-08 20:25:50 +0200

    Replaces initial state mock with real blank state.

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,15 @@ Vue.use(Vuex);
 
 const debug = process.env.NODE_ENV !== 'production';
 
-const state = { isAddingObject: false, canvas: {"objects":[{"_uid":"abc","type":"rect","originX":"left","originY":"top","left":114,"top":87,"width":20,"height":20,"fill":"red","stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeLineJoin":"miter","strokeMiterLimit":10,"scaleX":1,"scaleY":1,"angle":45,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"clipTo":null,"backgroundColor":"","fillRule":"nonzero","globalCompositeOperation":"source-over","transformMatrix":null,"skewX":0,"skewY":0,"rx":0,"ry":0},{"_uid":"def","type":"rect","originX":"left","originY":"top","left":200,"top":100,"width":20,"height":20,"fill":"green","stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeLineJoin":"miter","strokeMiterLimit":10,"scaleX":1,"scaleY":1,"angle":45,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"clipTo":null,"backgroundColor":"","fillRule":"nonzero","globalCompositeOperation":"source-over","transformMatrix":null,"skewX":0,"skewY":0,"rx":0,"ry":0}],"background":""}, meta: { activeObjectId: null }};
+const state = { 
+    isAddingObject: false, 
+    canvas: {
+      objects: []
+    },
+    meta: { 
+      activeObjectId: null 
+    }
+};
 
 export default new Vuex.Store({
   state,
